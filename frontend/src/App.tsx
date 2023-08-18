9487d2495b0ccc00b66aa0e2b89f02ad1fbd7f43
cleaninglab-jayd <jayd@cleaninglab.co.kr>
2023-08-19 07:28:45 +0900

    Feat: Global css 리셋

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,26 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { MainFooter } from './js/components/MainFooter';
+import styled from '@emotion/styled';
+import { Global } from '@emotion/react';
+import Reset from './reset';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/test" element={<>123</>}></Route>
-      </Routes>
+      <Global styles={Reset} />
+      <Container>
+        <Routes>
+          <Route path="/test" element={<>123</>}></Route>
+        </Routes>
+        <MainFooter />
+      </Container>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 380px;
+`;
