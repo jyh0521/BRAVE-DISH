c36bd963dc17626186a94e8e2b857e11a6dbe755
cleaninglab-jayd <jayd@cleaninglab.co.kr>
2023-08-19 09:28:03 +0900

    Feat: tsconfig 수정

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+ReactDOM.render(<App />, document.getElementById('root'));
