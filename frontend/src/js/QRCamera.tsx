d5f9f861d55a96d3ba2475ffdef5f4c82edf0911
cleaninglab-jayd <jayd@cleaninglab.co.kr>
2023-08-19 20:26:07 +0900

    Feat: 카메라 성공

--- frontend/src/js/QRCamera.tsx
+++ frontend/src/js/QRCamera.tsx
@@ -1,81 +1,29 @@
-// import { useEffect, useRef } from 'react';
-
-// export const QRCamera = () => {
-// let videoRef = useRef(null);
-// const getUserCamera = () => {
-//   // @ts-ignore
-//   navigator.mediaDevices
-//     .getUserMedia({
-//       video: { facingMode: 'environment' },
-//     })
-//     .then((stream) => {
-//       //비디오 tag에 stream 추가
-//       let video = videoRef.current;
-//       // @ts-ignore
-//       video.srcObject = stream;
-//       // @ts-ignore
-//       video.play();
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// };
-// useEffect(() => {
-//   getUserCamera();
-// }, [videoRef]);
-// return <video style={{ width: '100px', height: '100px' }} ref={videoRef}></video>;
-// };
-
-// import { useState } from 'react';
-// import { QrReader } from 'react-qr-reader';
-
-// export const QRCamera = () => {
-//   const [data, setData] = useState('No result');
-
-//   return (
-//     <>
-//       <QrReader
-//         onResult={(result, error) => {
-//           if (!!result) {
-//             // @ts-ignore
-//             setData(result?.text);
-//           }
-
-//           if (!!error) {
-//             console.info(error);
-//           }
-//         }}
-//         // @ts-ignore
-//         style={{ width: '100%' }}
-//       />
-//       <p>{data}</p>
-//     </>
-//   );
-// };
-
-import { useState } from 'react';
+import styled from '@emotion/styled';
 import { QrReader } from 'react-qr-reader';
+import { useNavigate } from 'react-router-dom';
 
 export const QRCamera = () => {
-  const [data, setData] = useState<any>('No result');
+  const navigate = useNavigate();
 
   return (
     <>
+      <CloseButton>✖︎</CloseButton>
       <QrReader
         constraints={{ facingMode: 'environment' }}
         onResult={(result, error) => {
           if (!!result) {
-            setData(result);
-            // console.log(result);
+            navigate('/mission/completed');
           }
-
           if (!!error) {
             console.info(error);
           }
         }}
         containerStyle={{ width: '100%' }}
       />
-      <p>{data}</p>
     </>
   );
 };
+
+const Header = styled.div``;
+
+const CloseButton = styled.div``;
